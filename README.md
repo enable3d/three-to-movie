53e56d12d3b8a52d311915215e75ec4d6b470561
yandeu <20306025+yandeu@users.noreply.github.com>
2021-08-01 16:59:34 +0200

    .

---
+++ README.md
@@ -0,0 +1,13 @@
+# Three.js to Movie
+
+Transforms your three.js scene into a movie.
+
+```bash
+# try in the browser (runs at 960x540 pixels)
+npm start
+
+# render to video (renders at 1280x720 pixels)
+npm run render
+
+# your movie will be the output.mp4 file
+```
---
+++ server/index.mjs
@@ -0,0 +1,81 @@
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import puppeteer from 'puppeteer'
+import util from 'util'
+import { ExpressListen } from '@yandeu/express-dev'
+import { exec as _exec } from 'child_process'
+import { existsSync } from 'fs'
+import { mkdir, rm, writeFile } from 'fs/promises'
+import { resolve, join } from 'path'
+
+const exec = util.promisify(_exec)
+
+// clean and make dir
+await rm(resolve('output.mp4'), { recursive: true, force: true })
+if (existsSync(resolve('screenshots'))) await rm(resolve('screenshots'), { recursive: true, force: true })
+await mkdir(resolve('screenshots'))
+
+const app = express()
+const port = 5402
+
+app.use(cors())
+app.use(bodyParser.json({ limit: '25mb' }))
+
+app.use('/scene', express.static(resolve('scene')))
+
+app.post('/', async (req, res) => {
+  const { img, frame } = req.body
+
+  try {
+    const data = img.replace(/^data:image\/png;base64,/, '')
+    const f = ('000' + frame).slice(-4)
+
+    await writeFile(resolve(join('screenshots', `frame${f}.png`)), data, 'base64')
+
+    if (frame > 0) {
+      process.stdout.moveCursor(0, -1) // up one line
+      process.stdout.clearLine(1) // from cursor to end
+    }
+
+    console.log('write frame', f)
+  } finally {
+    res.send()
+  }
+})
+
+const listen = new ExpressListen(app)
+
+listen.listen(port).then(async port => {
+  console.log(`Example app listening at http://localhost:${port}`)
+
+  // start puppeteer
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  // wait for the "DONE" log of the scene
+  page.on('console', async message => {
+    if (message.text() === 'DONE') {
+      await page.close()
+      await browser.close()
+
+      const cmd = `ffmpeg -y -r 30 -f image2 -i "${join('screenshots', 'frame%04d.png')}" -vcodec libx264 output.mp4`
+
+      console.log('')
+      console.log('> Running: ', cmd)
+      console.log('')
+
+      const { stdout, stderr } = await exec(cmd)
+      console.log('stdout:', stdout)
+      console.error('stderr:', stderr)
+
+      await listen.kill()
+
+      console.log('> DONE!')
+      process.exit(0)
+    }
+  })
+
+  await page.setUserAgent('puppeteer')
+  await page.goto(`http://localhost:${port}/scene`)
+})
